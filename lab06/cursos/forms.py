8d067bc27255f505004bb8ca8db52fdf0c5ed54b
RodrigoCLz <rcastillolaz@gmail.com>
2024-07-26 02:36:01 -0500

    p1

---
+++ lab06/cursos/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Docente, CursoLab
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password' ]
+
+class DocenteForm(forms.ModelForm):
+    class Meta:
+        model = Docente
+        fields = ['cod_docente', 'nombre','apellido_paterno','apellido_materno'] 
+        
+class CursoLabForm(forms.ModelForm):
+    class Meta:
+        model = CursoLab
+        fields = ['grupo','cod_docente','cod_salon']
