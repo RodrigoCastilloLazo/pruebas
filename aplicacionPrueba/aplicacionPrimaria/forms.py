3d9e69dd7b90371a5298596aa0fa803c3506f430
RodrigoCastilloLazo <169068422+RodrigoCastilloLazo@users.noreply.github.com>
2024-07-26 02:36:49 -0500

    Delete aplicacionPrueba directory

--- aplicacionPrueba/aplicacionPrimaria/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Alumno
-
-class AlumnoForm(forms.ModelForm):
-    class Meta:
-        model = Alumno
-        fields = ["cui", "nombre", "apellidos", "estado"]
